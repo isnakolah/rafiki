ab41214c8716763bc7a0e82bce87d16513b83987
Juma Allan <allanjuma@gmail.com>
2020-05-17 22:12:54 +0300

    updated char limit

--- data/model/message.go
+++ data/model/message.go
@@ -8,7 +8,7 @@ type Message struct {
 	MessageId       uuid.UUID `gorm:"type:uuid; primary_key" json:"message_id"`
 	UserId          uuid.UUID `gorm:"type:uuid; not null" json:"user_id"`
 	RecipientNumber string    `gorm:"type:varchar(15);"  json:"recipient_number"`
-	MessageBody     string    `gorm:"type:varchar(250);"  json:"message_body"`
+	MessageBody     string    `gorm:"type:varchar(5000);"  json:"message_body"`
 	MessageSent     bool      `gorm:"default:true" json:"message_sent"`
 	Response        string    `gorm:"type:varchar(500);"  json:"response"`
 	Rafiki
